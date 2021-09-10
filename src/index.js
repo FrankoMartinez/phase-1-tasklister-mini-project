ac47857d4699be1a1bd1d5c9b3d7e0785234676c
Francisco <frankmartinez4050@gmail.com>
2021-09-09 19:12:01 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const id = document.getElementById("new-task-description")
+    buildToDo(id.value);
+    form.reset();
+  })
 });
+
+function buildToDo(errand) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener("click", handleDelete)
+  btn.textContent = "X";
+  p.textContent = `${errand} `;
+  document.querySelector("#list").appendChild(p)
+  p.appendChild(btn)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
